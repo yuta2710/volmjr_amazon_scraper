7524bcf2664575722c2bb409b999d4b2598074f9
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-09-06 21:31:24 +0700

    Remove dist from the repository

--- dist/shared/supabase.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const supabase_js_1 = require("@supabase/supabase-js");
-console.log(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-const supabase = (0, supabase_js_1.createClient)(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-exports.default = supabase;
-//# sourceMappingURL=supabase.js.map
